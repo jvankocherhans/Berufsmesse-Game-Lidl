b0b98be178030d249cf6221d95e80cb9e13dc464
Jvan Kocherhans <kocherhansjvan@gmail.com>
2024-03-13 15:13:36 +0100

    Added logic for playerregistration

---
+++ SceneSwitcher.gd
@@ -0,0 +1,15 @@
+extends Control
+
+const ID_PREFIX: String = "LDL-"
+
+@onready var InputPlayerID: LineEdit = $Registration/InputPlayerID
+
+
+func _on_line_edit_text_submitted(input):
+	# checks if the prefix equals LDL-
+	if  input.begins_with(ID_PREFIX):
+		print(input.lstrip(ID_PREFIX))
+		visible = false
+	else:
+		print("wrong identification")
+		InputPlayerID.text = "";
