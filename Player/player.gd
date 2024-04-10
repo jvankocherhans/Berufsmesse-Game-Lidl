40bd64070e9793dd6021cfffbc4a4e08e6103436
Jvan Kocherhans <kocherhansjvan@gmail.com>
2024-04-10 07:01:13 +0200

    some changes

--- Player/player.gd
+++ Player/player.gd
@@ -5,7 +5,15 @@ class_name Player
 @export var speed = 2.0
 @export var gravity = 70
 
-@onready var pivot = $Pivot
+@onready var pivot = $Armature
+@onready var animation_player = $AnimationPlayer
+
+@export var isFreezed = true
+
+var score = 0;
+
+@onready var http_request = $HTTPRequest
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -15,26 +23,43 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-	apply_movement(input_vector)
+	
+	if Input.is_action_just_pressed("slide"):
+		slide(input_vector)
+	else:
+		apply_movement(input_vector)
+	
 	apply_gravity(delta)	
 	move_and_slide()
-
+		
 
 func get_input_vector():
 	var input_vector = Vector3.ZERO
 	input_vector.x = Input.get_action_strength("back") -  Input.get_action_strength("forward") 
 	input_vector.z = Input.get_action_strength("left") -  Input.get_action_strength("right") 
-	
 	return input_vector.normalized()
 	
 func apply_movement(input_vector):
-	velocity.x = input_vector.x * speed
-	velocity.z = input_vector.z * speed
+	if (!isFreezed):
+		velocity.x = input_vector.x * speed
+		velocity.z = input_vector.z * speed
+		animation_player.play("running")
 	
 	if input_vector != Vector3.ZERO:
 		pivot.look_at(position + input_vector, Vector3.UP)
 
+func slide(input_vector):
+	animation_player.play("slide")
+	
+
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+		
+## TEST FUnction
+
+func scoring():
+	score += 1
+
+
