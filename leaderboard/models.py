4b203d407ed198b0acab6750e1b99f0090b8f6da
Renenjr10 <114177876+Renenjr10@users.noreply.github.com>
2024-06-28 17:18:02 +0200

    Delete leaderboard directory

--- leaderboard/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class PlayerScore(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userID = db.Column(db.String(50), unique=True, nullable=False)
-    score = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f'<PlayerScore {self.userID}: {self.score}>'
